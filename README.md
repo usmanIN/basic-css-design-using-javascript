6896cd6a3ffbd8c274666560677b2efb157eeee9
usmanIN <61617956+usmanIN@users.noreply.github.com>
2022-02-12 22:03:03 +0530

    added readme

--- README.md
+++ README.md
@@ -1,7 +1,133 @@
 # js-design-libraries
  
-## General?
+## General
 
-This project help 
+This design library used for the purpose of reduce the time for code the basic design.
+
+Library is access without import other libarary.
+
+## Installation
+
+    a) npm module
+    b) cdn
+
+## Usage
 
 
+design("id-selector").funtion();
+
+## Example
+
+<span style="color: #f1f1f1">text : { } #apply the css value to format text</span>
+
+<table style="width:100%">
+    <tr>
+        <th>Function</th>
+        <th>Properties</th>
+        <th>default</th>
+    </tr>
+    <tr>
+        <td>.bubble()</td>
+        <td>
+<pre>{
+    count : "",
+    background : "",
+    size : ""
+}</pre></td>
+        <td>
+        count: 50<br/>
+        background: random_color</br>
+        size: random_size
+        </td>    
+    </tr>    
+    <tr>
+        <td>.typewriter()</td>
+        <td><pre>{
+    data : [],    
+    text : {}
+}</pre></td>
+        <td>&nbsp;</td>    
+    </tr>  
+    <tr>
+        <td>.tagCloud()</td>
+        <td>
+    <pre>{
+    data:[],
+    text:{}
+}
+</pre></td>
+        <td>&nbsp;</td>    
+    </tr>
+    <tr>
+        <td>.skillBar()</td>
+        <td>
+    <pre>{
+    skill:{ key:value, key:value },
+    text:{}
+    barColor:"",
+    background: "",
+    animate:true
+}</pre></td>
+        <td>
+        animate : false<br/>
+        background : grey<br/>
+        barColor : random_value
+        </td>    
+    </tr>  
+
+</table>
+
+
+## Example
+**bubble()**
+
+```javascript
+    design("#demo").bubble({
+        count:50,
+        background:"#072f5f", 
+        size:"50px"
+    });
+```
+
+
+**typewriter()**
+```javascript
+    design("#demo").typewriter({
+        data:["Developer","Freelancer"],
+        text:{
+            color:"red",            
+            "font-size":"80px",
+            "font-weight":"800"
+        }
+    });
+```    
+
+> tagCloud(): 
+
+```javascript    
+    design("#demo").tagCloud({
+        data:["Web Development","CSS","HTML"],
+        text:{
+            background:"yellow",
+            color:"green",
+            margin:"20px"
+        }
+    });
+```
+
+
+> skillBar()
+
+```javascript    
+    design("#demo").skillBar({
+        skill:{laptop: 5,mobile:7},
+        text:{
+            "font-size":"18px",
+            "font-weight":"800",
+            "text-transform":"uppercase"
+        },
+        barColor:"",
+        background: "",
+        animate:true
+    });
+```
--- design.js
+++ design.js
@@ -1,65 +1,65 @@
-
-let cssString = function(ObjectCSS){
-    ObjectCSS = Object.keys(ObjectCSS).map(function(key){
-        return key+":"+ObjectCSS[key]
-    }).join("; ");
-    return ObjectCSS;
-}
-
-let design = function(selectors){
+design = function(selectors){
     if(!selectors) return;
 
     'use strict';
     
     let self = document.querySelector(selectors);
-
     let isHaveStyleElementTag = false;
+    let cssString = function(ObjectCSS){
+        ObjectCSS = Object.keys(ObjectCSS).map(function(key){
+            return key+":"+ObjectCSS[key]
+        }).join("; ");
+        return ObjectCSS;
+    }
 
     if(document.querySelector("head").querySelectorAll("style").length > 0){
         isHaveStyleElementTag = true;
     }else{
-        let style = document.createElement("style");
-        style.setAttribute('type','text/css');
+        var styleElement = document.createElement("style");
+        styleElement.setAttribute('type','text/css');
     }
 
 
-    self.bubble = function(bubbleCount){
-        if(!bubbleCount) return;
+    self.bubble = function(options){
+        if(!options.count) return;
 
         if(isHaveStyleElementTag){
             document.querySelector("head").querySelector("style").append('@keyframes bubble{  0% { margin-top: 1000px;} 100% { margin-top: -100%;}');       
         }else{
-            style.innerText = `@keyframes bubble{  0% { margin-top: 1000px;} 100% { margin-top: -100%;}`;
-            document.querySelector("head").appendChild(style);
+            styleElement.innerText = `@keyframes bubble{  0% { margin-top: 1000px;} 100% { margin-top: -100%;}`;
+            document.querySelector("head").appendChild(styleElement);
         }
 
         let element = document.createElement("div");
         
-        for (var i = 0; i< bubbleCount; i++ ) {element.innerHTML+= "<span></span>";}   
+        for (var i = 0; i< options.count; i++ ) {element.innerHTML+= "<span></span>";}           
 
-        element.style.cssText = `position:fixed; top: 0; left:0; right:0; bottom:0; z-index:-1`; 
+        element.style.cssText = cssString({position:"fixed",inset:0, "z-index":-1}); 
         
         element.querySelectorAll("span").forEach(function(e){               		
-            let i =  Math.floor( 5+Math.random() * 20) + 'px';
+            let i =  (options.size)?options.size:Math.floor( 5+Math.random() * 20) + 'px';
         
-            red = Math.floor(Math.random()*255);
-            yellow = Math.floor(Math.random()*255);
-            blue = Math.floor(Math.random()*255);
-     
-            let css = cssString({
+            if(!options.background){
+                red = Math.floor(Math.random()*255);
+                yellow = Math.floor(Math.random()*255);
+                blue = Math.floor(Math.random()*255);
+                background = "rgba("+red+","+yellow+","+blue+")";
+            }else{
+                background = options.background;
+            }
+                 
+            e.style.cssText = cssString({
                 "position": "absolute","height":i,"width":i,
                 "left":Math.floor(Math.random() * innerWidth) +"px",
                 "top":Math.floor(Math.random() * innerWidth) +"px",
                 "animation": "bubble 25s linear infinite",
-                "border-radius":"50%","background":"rgba("+red+","+yellow+","+blue+")",
+                "border-radius":"50%","background":background,
                 "animation-duration":Math.floor(Math.random()*10+10)+"s"
             });
-            e.style.cssText = css;
             
         });       	
 
-        self.append(element);
-        return self;
+        self.appendChild(element); 
     }
 
     self.skillBar = function(options){
@@ -68,15 +68,13 @@ let design = function(selectors){
         if(isHaveStyleElementTag){
             document.querySelector("head").querySelector("style").append(`.barLength::before{ content: ""; padding: 5px;  position: absolute; right:0; bottom: 6px; background:#494d5f; float: right;  border-radius: 50%;}`);        
         }else{            
-            style.innerText = `.barLength::before{ content: ""; padding: 5px;  position: absolute; right:0; bottom: 6px; background:#494d5f; float: right;  border-radius: 50%;}`;
-            document.querySelector("head").appendChild(style);
+            styleElement.innerText = `.barLength::before{ content: ""; padding: 5px;  position: absolute; right:0; bottom: 6px; background:#494d5f; float: right;  border-radius: 50%;}`;
+            document.querySelector("head").appendChild(styleElement);
         }
-
     
         let skillbar = cssString({"width": "100%", "margin-bottom": "5px",});    
         let skillName = {"display": "inline-block"};
         let skillValue ={"float":"right"};
-
         
         if(options.text){        
             for (const key in options.text) {            
@@ -102,21 +100,9 @@ let design = function(selectors){
         }
         
         self.innerHTML = container; 
-        self.querySelectorAll(".skillbar").forEach(function(e){
-            let text = e.querySelectorAll("span")[1].innerText.replace("%",'');
-            let i = 0;
-            let counter = setInterval(function(){
-                if(i<=parseInt(text)){
-                    e.querySelectorAll("span")[1].innerText = i+"%";
-                    i++;
-                }else{
-                    clearInterval(counter);
-                }
-            },25);    
-        });
 
-        
-        let barContainer = cssString({"display" : "block", "width" : "100%","background" : (!options.barBackground && options.barBackground=="")?"#f1f1f1":options.barBackground});
+        let barContainer = cssString({"display" : "block", "width" : "100%","background" : (!options.background)?"#f1f1f1":options.background});
+        console.log(barContainer);
     
         self.querySelectorAll(".barContainer").forEach(function(e){ e.style.cssText = barContainer; });
 
@@ -130,33 +116,80 @@ let design = function(selectors){
  
            let percentage = e.querySelectorAll("span")[1].innerText.replace("%","");
            
-           barLength = cssString({"background":(options.barColor && options.barColor!=="")?options.barColor:"rgba("+red+","+yellow+","+blue+","+"0.8)",width:"0%","padding":"10px 0"});
+           barLength = cssString({
+               "background":(options.barColor && options.barColor!=="")?options.barColor:"rgba("+red+","+yellow+","+blue+","+"0.8)",
+               width:"0%","padding":"10px 0"
+            });
 
            e.querySelector(".barLength").style.cssText =  barLength;       
-           let i = 0;
-           let counter = setInterval(function(){
-               if(i<=percentage){
-                e.querySelector(".barLength").style.width = i+"%";
-                i++;
-               }else{
-                   clearInterval(counter);
-               }
-           },25);       
+           if(options.animate){                    
+                let i = 0;
+                let counter = setInterval(function(){
+                    if(i<=percentage){
+                        e.querySelector(".barLength").style.width = i+"%";
+                        e.querySelectorAll("span")[1].innerText = i+"%";
+                        i++;
+                    }else{
+                        clearInterval(counter);
+                    }
+                },30);                      
+           }else{
+                e.querySelector(".barLength").style.width = percentage+"%";
+           }
 
        });
- 
-       return self;
+
     }
 
     
+    self.tagCloud = function(options){
+        if(!options) return;    
+        if(!options.data) return;
+        let style = {"padding":"10px 20px","background":"black","color":"#ffffff"};        
+        if(options.text){        
+            for (const key in options.text) {            
+                if(options.text[key]!==""){ style[key] = options.text[key]; }            
+            }
+        }
+        options.data.forEach(element => {
+            self.innerHTML+= ("<span class='tag-badge' style='"+cssString(style)+"'>" + element + "</span>");            
+        });
+    }
 
 
-
-
-
-
-
-
-    return self;
+    self.typewriter = function(options){
     
+        if(!options.data) return;
+
+        let span = document.createElement("span");
+        span.setAttribute("class","dataText");
+        self.appendChild(span);
+        
+        if(options.text){
+            document.querySelector("span.dataText").style.cssText = cssString(options.text);
+        }
+
+        function typeWriter(text, i, fnCallback) {    
+          if (i < (text.length)) {      
+            document.querySelector("span.dataText").innerHTML = text.substring(0, i+1);            
+                setTimeout(() => {  typeWriter(text, i + 1, fnCallback) }, 100);
+            }
+            else if (typeof fnCallback == 'function') {   
+                setTimeout(fnCallback, 700);
+            }
+        }
+        
+        function StartTextAnimation(i) {
+          if (typeof options.data[i] == 'undefined'){
+              setTimeout(function() {   StartTextAnimation(0) }, 1000);
+          }      
+          if (i < options.data.length) {
+                typeWriter(options.data[i], 0, () =>{   StartTextAnimation(i + 1) });
+          }    
+        }
+
+       StartTextAnimation(0);   
+    }
+
+    return self;    
 }
